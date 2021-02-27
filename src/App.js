e836119f692091f3107d311aa0abed05b8e583b4
unknown <arus.sayamyan@gmail.com>
2021-02-27 12:18:19 +0400

    implemented search and its logic

--- src/App.js
+++ src/App.js
@@ -1,37 +1,61 @@
-import './app.scss';
+import React from "react";
+import "./app.scss";
 
 class App extends React.component {
   state = {
     search: "Narek",
-    cats: ["Marvin", "Darvin", "Harry Poter"]
-  }
-
-  handleChange = (event) => {
-    const value = event.target.value;
-    this.setState({search: value});
+    cats: [
+      { name: "Marvin", email: "marvin@mail.com" },
+      { name: "Darvin", email: "marvin@mail.com" },
+      { name: "Harry Poter", email: "marvin@mail.com" },
+    ],
   };
 
+  handleChange = (event) => {
+    //? inputValue 
+    const searchValue = event.target.value;
+    //? inputValue set inputValue to state
+    this.setState({ search: searchValue });
+  };
 
   render() {
-
-    console.log(this.state.cats)
-    return (
-    <div className="app">
-      <h1 className = "app__header">Catty shop</h1>
-     <div className="app__search">
-       <div>{this.state.name}</div>
-
-       <input className="app__search-input" type="text" name="search" placeholder="Search cats"/>
-    </div>
-
-    <main className="app__main">
-         {this.state.cats.map(cat =>)}
-    </main>
-    </div>
-  
+    const mySearchInputValue = this.state.search;
     
-  );
-}
+const filteredCatsData = this.state.cats.filter((cat) => {
+  return cat.name.includes(mySearchInputValue);
+});
+
+console.log(filteredCatsData);
+
+    return (
+      <div className="app">
+        <h1 className="app__header">Catty shop</h1>
+        <div className="app__search">
+          <div>{this.state.name}</div>
+
+          <input
+            className="app__search-input"
+            type="text"
+            name="search"
+            placeholder="Search cats"
+          />
+        </div>
+
+        <main className="app__main">
+          <ul>
+            {mySearchInputValue.map((cat, idx) => {
+              return (
+                <li className="item__header">
+                  <div className="item">{cat.name}</div>
+                  <div>{cat.email}</div>
+                </li>
+              );
+            })}
+          </ul>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
