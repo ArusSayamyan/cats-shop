f475fe8e11161196cc2440cdfdd27e1ad63082b1
unknown <arus.sayamyan@gmail.com>
2021-02-27 12:15:06 +0400

    implement ed search and its logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.component {
+  state = {
+    search: "Narek",
+    cats: ["Marvin", "Darvin", "Harry Poter"]
+  }
+
+  handleChange = (event) => {
+    const value = event.target.value;
+    this.setState({search: value});
+  };
+
+
+  render() {
+
+    console.log(this.state.cats)
+    return (
+    <div className="app">
+      <h1 className = "app__header">Catty shop</h1>
+     <div className="app__search">
+       <div>{this.state.name}</div>
+
+       <input className="app__search-input" type="text" name="search" placeholder="Search cats"/>
     </div>
+
+    <main className="app__main">
+         {this.state.cats.map(cat =>)}
+    </main>
+    </div>
+  
+    
   );
+}
 }
 
 export default App;
